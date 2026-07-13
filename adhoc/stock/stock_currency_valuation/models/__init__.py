50d02108c70e900fb5311813acfb609c891d8479
Javier Pepe <javierpepe@gmail.com>
2026-07-13 17:11:22 -0300

    Agrego dependencia mercado pago

--- adhoc/stock/stock_currency_valuation/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import product_category
-from . import stock_valuation_layer
-from . import product_product
-from . import product_template
-from . import stock_move
-from . import stock_landed_cost
-from . import stock_picking
--- adhoc/stock/stock_ux/wizards/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import stock_operation_wizard
-from . import stock_label_type
