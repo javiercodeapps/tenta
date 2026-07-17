82004643169d570ed3e709b910ee9e41c8b0f606
Javier Pepe <javierpepe@gmail.com>
2026-07-17 19:10:24 -0300

    Repos actualizados

--- adhoc/stock/delivery_ux/models/delivery_carrier.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class DeliveryCarrier(models.Model):
-    _inherit = "delivery.carrier"
-
-    partner_id = fields.Many2one("res.partner", string="Carrier Address", help="Utilizado para el remito.")
